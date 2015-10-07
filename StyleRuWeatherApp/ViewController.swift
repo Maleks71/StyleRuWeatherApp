55cacede457d2d3edd1d9e0ebdcde5fb330c8763
= <=>
2015-10-08 01:05:15 +0300

    frst

---
+++ StyleRuWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StyleRuWeatherApp
+//
+//  Created by Григорий Стригунов on 07.10.15.
+//  Copyright © 2015 Grigory Strigunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
