8819493729742ea0c08c45e201bb6af491cb8ae7
ownmas <ownmas@gmail.com>
2015-10-08 14:37:18 +0300

    Add class WeatherData and Alamofire request tests

--- StyleRuWeatherApp/ViewController.swift
+++ StyleRuWeatherApp/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let params = ["q" : "Moscow"]
+        
+        Alamofire.request(.GET, "http://api.openweathermap.org/data/2.5/weather", parameters: params)
+            .responseJSON { response in
+                //print("1. \(response.request)\n\n")  // original URL request
+                //print("2. \(response.response)\n\n") // URL response
+                //print("3. \(response.data)\n\n")     // server data
+                print("4. \(response.result)\n\n")   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)\n\n")
+                    
+                    let data = JSON as! [String : AnyObject]
+                    let name = data["name"]
+                    print("data: \(name)")
+                }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ StyleRuWeatherApp/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  StyleRuWeatherApp
+//
+//  Created by Григорий Стригунов on 08.10.15.
+//  Copyright © 2015 Grigory Strigunov. All rights reserved.
+//
+
+import Foundation
+
+
+class WeatherData {
+    
+    var temp: Int?
+    var clouds: Int?
+    var windSpeed: Int?
+    var pressure: Int?
+    
+    init() {
+        
+        //self.temp = 0
+    }
+    
+}
